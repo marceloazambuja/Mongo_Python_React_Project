740c9ffe02bf9a696854ffe91834105a528e8243
Marcelo Cunha de Azambuja <marcelo.azambuja@gmail.com>
2019-06-27 10:27:20 -0300

    a lot of user management accounts methods: add, delete, login, ...

--- mflix/db.py
+++ mflix/db.py
@@ -156,7 +156,7 @@ def get_movies_faceted(filters, page, movies_per_page):
     # pipeline.extend(...)
 
     pipeline.extend([skip_stage,limit_stage,facet_stage])
-    
+
     try:
         movies = list(db.movies.aggregate(pipeline, allowDiskUse=True))[0]
         count = list(db.movies.aggregate(counting, allowDiskUse=True))[
@@ -395,7 +395,7 @@ def get_user(email):
     """
     # TODO: User Management
     # Retrieve the user document corresponding with the user's email.
-    return db.users.find_one({ "some_field": "some_value" })
+    return db.users.find_one({ "email": email })
 
 
 def add_user(name, email, hashedpw):
@@ -416,12 +416,17 @@ def add_user(name, email, hashedpw):
         # Insert a user with the "name", "email", and "password" fields.
         # TODO: Durable Writes
         # Use a more durable Write Concern for this operation.
-        db.users.insert_one({
-            "name": "mongo",
-            "email": "mongo@mongodb.com",
-            "password": "flibbertypazzle"
-        })
-        return {"success": True}
+
+        find_result = db.users.find_one({"email":email},{"_id":0,"email":1})
+        if db.users.find_one({"email":email}) is None:
+            db.users.insert_one({
+            "name": name,
+            "email": email,
+            "password": hashedpw
+            })
+            return {"success": True}
+        else:
+            return {"error": "A user with the given email already exists."}
     except DuplicateKeyError:
         return {"error": "A user with the given email already exists."}
 
@@ -438,8 +443,8 @@ def login_user(email, jwt):
         # Use an UPSERT statement to update the "jwt" field in the document,
         # matching the "user_id" field with the email passed to this function.
         db.sessions.update_one(
-            { "some_field": "some_value" },
-            { "$set": { "some_other_field": "some_other_value" } }
+            { "user_id": email },
+            { "$set": { "jwt": jwt } }, upsert=True
         )
         return {"success": True}
     except Exception as e:
@@ -456,7 +461,7 @@ def logout_user(email):
     try:
         # TODO: User Management
         # Delete the document in the `sessions` collection matching the email.
-        db.sessions.delete_one({ "some_field": "some_value" })
+        db.sessions.delete_one({ "user_id": email })
         return {"success": True}
     except Exception as e:
         return {"error": e}
@@ -471,7 +476,7 @@ def get_user_session(email):
     try:
         # TODO: User Management
         # Retrieve the session document corresponding with the user's email.
-        return db.sessions.find_one({ "some_field": "some_value" })
+        return db.sessions.find_one({ "user_id": email })
     except Exception as e:
         return {"error": e}
 
@@ -484,8 +489,8 @@ def delete_user(email):
     try:
         # TODO: User Management
         # Delete the corresponding documents from `users` and `sessions`.
-        db.sessions.delete_one({ "some_field": "some_value" })
-        db.users.delete_one({ "some_field": "some_value" })
+        db.sessions.delete_one({ "user_id": email })
+        db.users.delete_one({ "email": email })
         if get_user(email) is None:
             return {"success": True}
         else:
